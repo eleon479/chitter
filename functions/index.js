828860ce4da042afa77758045dad07ea7bfdbb62
eleon479 <eleon479@gmail.com>
2022-12-03 04:05:23 -0500

    config firebase emulators + cloud functions

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
+// admin.firestore().settings({ ignoreUndefinedProperties: true });
 
 exports.addMessage = functions.https.onRequest(async (req, res) => {
   const original = req.query.text;
@@ -13,3 +14,13 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
+
+exports.makeUppercase = functions.firestore
+  .document("/messages/{documentId}")
+  .onCreate((snap, context) => {
+    const original = snap.data().original;
+    functions.logger.log("Uppercasing", context.params.documentId, original);
+    const uppercase = original.toUpperCase();
+
+    return snap.ref.set({ uppercase }, { merge: true });
+  });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,9 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TimelineService } from './services/timeline.service';
+import { environment } from '../environments/environment';
 
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { environment } from '../environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireAuthGuardModule } from '@angular/fire/compat/auth-guard';
-
 import {
   AngularFireAnalyticsModule,
   APP_NAME,
@@ -20,6 +15,24 @@ import {
   UserTrackingService,
   COLLECTION_ENABLED,
 } from '@angular/fire/compat/analytics';
+import {
+  AngularFirestoreModule,
+  USE_EMULATOR as USE_FIRESTORE_EMULATOR,
+  SETTINGS as FIRESTORE_SETTINGS,
+} from '@angular/fire/compat/firestore';
+import {
+  AngularFireAuthModule,
+  USE_DEVICE_LANGUAGE,
+  USE_EMULATOR as USE_AUTH_EMULATOR,
+} from '@angular/fire/compat/auth';
+import {
+  AngularFireFunctionsModule,
+  USE_EMULATOR as USE_FUNCTIONS_EMULATOR,
+} from '@angular/fire/compat/functions';
+import { AngularFireAuthGuardModule } from '@angular/fire/compat/auth-guard';
+
+import { TimelineService } from './services/timeline.service';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,12 +40,42 @@ import {
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule, // for firestore
+    // AngularFirestoreModule,
+    AngularFirestoreModule.enablePersistence({ synchronizeTabs: true }),
     AngularFireAnalyticsModule,
     AngularFireAuthModule,
     AngularFireAuthGuardModule,
+    AngularFireFunctionsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
   ],
-  providers: [TimelineService, UserTrackingService, ScreenTrackingService],
+  providers: [
+    TimelineService,
+    UserTrackingService,
+    ScreenTrackingService,
+    {
+      provide: FIRESTORE_SETTINGS,
+      useValue: { ignoreUndefinedProperties: true },
+    },
+    { provide: ANALYTICS_DEBUG_MODE, useValue: true },
+    { provide: COLLECTION_ENABLED, useValue: true },
+    {
+      provide: USE_AUTH_EMULATOR,
+      useValue: environment.useEmulators
+        ? ['http://localhost:9099']
+        : undefined,
+    },
+    {
+      provide: USE_FIRESTORE_EMULATOR,
+      useValue: environment.useEmulators ? ['localhost', 8080] : undefined,
+    },
+    {
+      provide: USE_FUNCTIONS_EMULATOR,
+      useValue: environment.useEmulators ? ['localhost', 5001] : undefined,
+    },
+    { provide: USE_DEVICE_LANGUAGE, useValue: true },
+    { provide: APP_VERSION, useValue: '0.0.4' },
+    { provide: APP_NAME, useValue: 'Chitter' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
